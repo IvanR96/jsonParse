d69dac77fdfc8bd0775cac028c7c4ca1fba5a960
IvanR96 <irios96@hotmail.com>
2024-01-27 22:47:29 -0800

    stringify comment added

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const text = '{"name":"Ivan", "age":"27", "city":"Santa Ana"}';
 const obj = JSON.parse(text);
 
 document.getElementById('demo').innerHTML = obj.name + ", " + "is from " + obj.city + " and is " + obj.age + " years old.";
+
+
+
+// stringify using JSON.stringify(obj) would put array into string like this -----> {"name":"John","age":30,"city":"New York"}
